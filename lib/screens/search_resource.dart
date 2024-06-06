c0ea5ff31d1e0d070d6a65304e7011f2b8ff0d03
Godagesasindu <godagesasindu@gmail.com>
2024-06-06 23:54:00 +0530

    resources grid view

--- lib/screens/search_resource.dart
+++ lib/screens/search_resource.dart
@@ -1,102 +1,128 @@
+// import 'package:easylibro_app/widgets/my_resource.dart';
+// import 'package:easylibro_app/widgets/resource_card.dart';
 // import 'package:easylibro_app/widgets/search__bar.dart';
 // import 'package:flutter/material.dart';
 
-// class SearchResource extends StatelessWidget {
+// class SearchResource extends StatefulWidget {
 //   const SearchResource({super.key});
 
-//   // ignore: non_constant_identifier_names
+//   @override
+//   // ignore: library_private_types_in_public_api
+//   _SearchResourceState createState() => _SearchResourceState();
+// }
+
+// class _SearchResourceState extends State<SearchResource> {
+//   int isSelected = 0;
 
 //   @override
 //   Widget build(BuildContext context) {
-//     //  TextEditingController Searchbarcontroller = TextEditingController();
-
 //     return SafeArea(
 //       child: Scaffold(
-//           body: Padding(
-//         padding: const EdgeInsets.all(20),
-//         child: Column(
-//           children: [
-//             Row(
-//                 mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                 crossAxisAlignment: CrossAxisAlignment.start,
+//           backgroundColor: const Color(0xFFF7F8FD),
+//           //backgroundColor: Color.fromARGB(255, 17, 45, 188),
+//           body: SingleChildScrollView(
+//             child: Padding(
+//               padding: const EdgeInsets.all(20),
+//               child: Column(
 //                 children: [
-//                   // Search_Bar(hintText: "Search books", controller: Searchbarcontroller),
-//                   Search_Bar(hintText: "Search Resources"),
-//                   Padding(
-//                     padding: const EdgeInsets.only(left: 20),
-//                     child: Container(
-//                       height: 50,
-//                       width: 50,
-//                       decoration: BoxDecoration(
-//                           color: const Color(0xFF0D4065),
-//                           borderRadius: BorderRadius.circular(20)),
-//                       child: const Icon(
-//                         Icons.tune_outlined,
-//                         color: Colors.white,
-//                         size: 30,
-//                       ),
+//                   Row(
+//                       mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                       crossAxisAlignment: CrossAxisAlignment.start,
+//                       children: [
+//                         Search_Bar(hintText: "Search Resources"),
+//                         Padding(
+//                           padding: const EdgeInsets.only(left: 20),
+//                           child: Container(
+//                             height: 40,
+//                             width: 40,
+//                             decoration: BoxDecoration(
+//                                 color: const Color(0xFF0D4065),
+//                                 borderRadius: BorderRadius.circular(15)),
+//                             child: const Icon(
+//                               Icons.tune_outlined,
+//                               color: Colors.white,
+//                               size: 25,
+//                             ),
+//                           ),
+//                         )
+//                       ]),
+//                   SingleChildScrollView(
+//                     scrollDirection: Axis.horizontal,
+//                     child: Row(
+//                       mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                       children: [
+//                         _buildResourceCategory(index: 0, name: "All Resources"),
+//                         _buildResourceCategory(index: 1, name: "Latest"),
+//                         _buildResourceCategory(index: 2, name: "Popular"),
+//                         _buildResourceCategory(index: 3, name: "Recommended"),
+//                         _buildResourceCategory(index: 4, name: "Popular"),
+//                       ],
 //                     ),
-//                   )
-//                 ]),
-//             Padding(
-//               padding: const EdgeInsets.only(top: 20),
-//               child: Container(
-//                 height: 60,
-//                 child: SingleChildScrollView(
-//                   scrollDirection: Axis.horizontal,
-//                   child: Row(
-//                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+//                   ),
+//                   const SizedBox(height:20),
+//                   Column(
 //                     children: [
-//                       SizedBox(width: 20),
-//                       _allitem(""),
-//                       SizedBox(width: 20),
-//                       _singleitem("Popular"),
-//                       SizedBox(width: 20),
-//                       _singleitem("Latest"),
-//                       SizedBox(width: 20),
-//                       _singleitem("Latest"),
-                    
+//                       Expanded(child: isSelected ==0? _buildAllResources():_buillatestResources())
 //                     ],
 //                   ),
-//                 ),
+
+//                 ],
 //               ),
-//             )
-//           ],
-//         ),
-//       )),
+//             ),
+//           )),
 //     );
 //   }
-// }
-
-// Widget _allitem(String searchtext) {
-//   return ElevatedButton(
-//     style: const ButtonStyle(
-//       backgroundColor: WidgetStatePropertyAll(Color(0xFF0D4065)),
-//     ),
-//     child: const Text("All",style: const TextStyle(
-//           fontSize: 14,
-//           color: Color.fromARGB(255, 255, 255, 255),
+//   _buildResourceCategory({required int index,required String name}) => Container(
+//         width: 105,
+//         height: 40,
+//         margin: const EdgeInsets.only(top: 20, right: 30),
+//         padding: const EdgeInsets.all(8),
+//         alignment: Alignment.center,
+//         decoration: BoxDecoration(
+//           borderRadius: BorderRadius.circular(15),
+//           color: isSelected == index
+//               ? const Color(0xFF0D4065) : const Color(0xFFE1E2E2),
+//         ),
+//         child: Text(name,style: TextStyle(
+//           fontSize: 13,
+//           color: isSelected== index ? Colors.white : const Color(0xFF3F3D3D),
 //           fontFamily: "Inter",
-//           fontWeight: FontWeight.w500),),
-//     onPressed: () {},
-//   );
+//           fontWeight: FontWeight.w500,
+//         ),),
+//       );
+
+//   _buildAllResources() => GridView.builder(
+//       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//         crossAxisCount: 2,
+//         childAspectRatio:( 100 / 140),
+//         crossAxisSpacing: 12,
+//         mainAxisSpacing: 10
+//       ),
+//       scrollDirection: Axis.vertical,
+//       itemCount: MyResources.allResources.length,
+//       itemBuilder: (context, index) {
+//         final allResources = MyResources.allResources[index];
+//         return ResourceCard(resource: allResources);
+//       });
+
+//   _buillatestResources() => GridView.builder(
+//       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//         crossAxisCount: 2,
+//         childAspectRatio:( 100 / 140),
+//         crossAxisSpacing: 12,
+//         mainAxisSpacing: 10
+//       ),
+//       scrollDirection: Axis.vertical,
+//       itemCount: MyResources.latest.length,
+//       itemBuilder: (context, index) {
+//         final latest = MyResources.latest[index];
+//         return ResourceCard(resource: latest);
+//       });
 // }
 
-// Widget _singleitem(String searchtext) {
-//   return ElevatedButton(
-//     child: Text(
-//       searchtext,
-//       style: const TextStyle(
-//           fontSize: 14,
-//           color: Color(0xFF080C27),
-//           fontFamily: "Inter",
-//           fontWeight: FontWeight.w500),
-//     ),
-//     onPressed: () {},
-//   );
-// }
-
-
+import 'package:easylibro_app/widgets/my_resource.dart';
+import 'package:easylibro_app/widgets/resource_card.dart';
 import 'package:easylibro_app/widgets/search__bar.dart';
 import 'package:flutter/material.dart';
 
@@ -108,89 +134,132 @@ class SearchResource extends StatefulWidget {
 }
 
 class _SearchResourceState extends State<SearchResource> {
-  String selectedCategory = "All";
+  int isSelected = 0;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Search_Bar(hintText: "Search Resources"),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: Container(
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
+        backgroundColor: const Color(0xFFF7F8FD),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min, // Set mainAxisSize to min
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Search_Bar(hintText: "Search Resources"),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
                           color: const Color(0xFF0D4065),
-                          borderRadius: BorderRadius.circular(20)),
-                      child: const Icon(
-                        Icons.tune_outlined,
-                        color: Colors.white,
-                        size: 30,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: const Icon(
+                          Icons.tune_outlined,
+                          color: Colors.white,
+                          size: 25,
+                        ),
                       ),
                     ),
-                  )
-                ]),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: SizedBox(
-                height: 60,
-                child: SingleChildScrollView(
+                  ],
+                ),
+                SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      const SizedBox(width: 20),
-                      _categoryItem("All"),
-                      const SizedBox(width: 20),
-                      _categoryItem("Popular"),
-                      const SizedBox(width: 20),
-                      _categoryItem("Latest"),
-                      const SizedBox(width: 20),
-                      _categoryItem("Trending"),
-                      const SizedBox(width: 20),
-                      _categoryItem("Recommended"),
+                      _buildResourceCategory(index: 0, name: "All Resources"),
+                      _buildResourceCategory(index: 1, name: "Latest"),
+                      _buildResourceCategory(index: 2, name: "Popular"),
+                      _buildResourceCategory(index: 3, name: "Recommended"),
+                      _buildResourceCategory(index: 4, name: "Popular"),
                     ],
                   ),
                 ),
-              ),
-            )
-          ],
+                const SizedBox(height: 20),
+                // Flexible(
+                //   fit: FlexFit.loose,
+                //   child: isSelected == 0
+                //       ? _buildAllResources()
+                //       : _buildLatestResources(),
+                // ),
+                Expanded(child: _buildAllResources())
+              ],
+            ),
+          ),
         ),
-      )),
+      ),
     );
   }
 
-  Widget _categoryItem(String category) {
-    bool isSelected = selectedCategory == category;
-    return ElevatedButton(
-      style: ButtonStyle(
-        backgroundColor: WidgetStateProperty.all(
-          isSelected ? const Color(0xFF0D4065) : Colors.grey,
+  Widget _buildResourceCategory({required int index, required String name}) =>
+      GestureDetector(
+        onTap: () {
+          setState(() {
+            isSelected = index;
+          });
+        },
+        child: Container(
+          width: 105,
+          height: 40,
+          margin: const EdgeInsets.only(top: 20, right: 30),
+          padding: const EdgeInsets.all(8),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: isSelected == index
+                ? const Color(0xFF0D4065)
+                : const Color(0xFFE1E2E2),
+          ),
+          child: Text(
+            name,
+            style: TextStyle(
+              fontSize: 13,
+              color:
+                  isSelected == index ? Colors.white : const Color(0xFF3F3D3D),
+              fontFamily: "Inter",
+              fontWeight: FontWeight.w500,
+            ),
+          ),
         ),
-      ),
-      child: Text(
-        category,
-        style: TextStyle(
-          fontSize: 14,
-          color: isSelected ? Colors.white : const Color(0xFF080C27),
-          fontFamily: "Inter",
-          fontWeight: FontWeight.w500,
+      );
+
+  Widget _buildAllResources() => GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 12,
+            mainAxisSpacing: 10,
+            childAspectRatio: 4 / 5),
+        // shrinkWrap: true,
+        // physics: const NeverScrollableScrollPhysics(),
+        scrollDirection: Axis.vertical,
+        itemCount: MyResources.allResources.length,
+        itemBuilder: (context, index) {
+          final allResources = MyResources.allResources[index];
+          return ResourceCard(resource: allResources);
+        },
+      );
+
+  Widget _buildLatestResources() => GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: (100 / 140),
+          crossAxisSpacing: 12,
+          mainAxisSpacing: 100,
         ),
-      ),
-      onPressed: () {
-        setState(() {
-          selectedCategory = category;
-        });
-      },
-    );
-  }
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: MyResources.latest.length,
+        itemBuilder: (context, index) {
+          final latest = MyResources.latest[index];
+          return ResourceCard(resource: latest);
+        },
+      );
 }
--- lib/widgets/search__bar.dart
+++ lib/widgets/search__bar.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable, camel_case_types
 class Search_Bar extends StatefulWidget {
-  
   String? hintText;
-  ValueChanged<String>? onChanged;  
+  ValueChanged<String>? onChanged;
+  //WidgetStateProperty<TextStyle?>? textStyle;
 
-  Search_Bar(
-      {super.key,
-      required this.hintText,
-      this.onChanged,
+  Search_Bar({
+    super.key,
+    required this.hintText,
+    this.onChanged,
+   // this.textStyle,
+
   });
 
   @override
@@ -22,16 +24,15 @@ class _Search_BarState extends State<Search_Bar> {
   Widget build(BuildContext context) {
     return SearchBar(
       constraints: const BoxConstraints(
-      minHeight: 50,
-      maxWidth:280, 
-    ),
+        minHeight: 40,
+        maxWidth: 280,
+      ),
       leading: const Icon(Icons.search_outlined),
       hintText: widget.hintText,
+      //textStyle: widget.textStyle,
       backgroundColor: const WidgetStatePropertyAll(Colors.white),
-      shape: WidgetStateProperty.all(const ContinuousRectangleBorder(
-      borderRadius: BorderRadius.all(Radius.circular(30)),     
-    )),
-    elevation: WidgetStateProperty.all(2),
+       
+      elevation: WidgetStateProperty.all(2),
     );
   }
 }
