c697e1d608b4dada6b62219d785db5cf025d6ed9
Nipuni31 <anuradhagangodawilann@gmail.com>
2024-06-08 01:26:37 +0530

    setting section completed

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -7,7 +7,18 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-     List<Item> _data = generateItems();
+  final _formKey = GlobalKey<FormState>();
+  String _currentPassword = '';
+  String _newPassword = '';
+  String _confirmPassword = '';
+  String _currentEmail = '';
+  String _newEmail = '';
+  bool _notificationsEnabled = true;
+  bool _darkModeEnabled = false;
+  double _volumeLevel = 0.5;
+  String _language = 'English';
+  final String _emailPattern =
+      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,108 +43,323 @@ class _SettingsPageState extends State<SettingsPage> {
               "Settings",
               style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
             ),
-            SizedBox(
-              height: 40,
-            ),
-            Row(
-              children: [
-                Icon(
-                  Icons.person,
-                    color: const Color(0xFF0D4065)
-                ),
-                SizedBox(
-                  width: 8,
-                ),
-                Text(
-                  "Account",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            Divider(
-              height: 15,
-              thickness: 2,
-            ),
 
-            ExpansionPanelList(
-              expansionCallback: (int index, bool isExpanded) {
-                setState(() {
-                  _data[index].isExpanded = !isExpanded;
-                });
-              },
-              children: _data.map<ExpansionPanel>((Item item) {
-                return ExpansionPanel(
-                  headerBuilder: (BuildContext context, bool isExpanded) {
-                    return ListTile(
-                      title: Text(item.headerValue),
-                    );
-                  },
-                  body: ListTile(
-                    title: Text(item.expandedValue),
-                    subtitle: item.headerValue == "Privacy and security"
-                        ? Row(
-                      children: [
-                        Icon(Icons.volume_up_outlined,
-                            color: const Color(0xFF0D4065)),
-                        SizedBox(width: 8),
-                        Text('Additional Settings')
-                      ],
-                    )
-                        : null,
-                  ),
-                  isExpanded: item.isExpanded,
-                );
-              }).toList(),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Row(
-              children: [
-                Icon(
-                  Icons.volume_up_outlined,
-                    color: const Color(0xFF0D4065)
-                ),
-                SizedBox(
-                  width: 8,
-                ),
-                Text(
-                  "Notifications",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            Divider(
-              height: 15,
-              thickness: 2,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            buildNotificationOptionRow("New for you", true),
-            buildNotificationOptionRow("Account activity", true),
-            buildNotificationOptionRow("Opportunity", false),
-            SizedBox(
-              height: 50,
-            ),
-            Center(
-              child: OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(horizontal: 40),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-                onPressed: () {},
-                child: Text("SIGN OUT",
-                    style: TextStyle(
-                        fontSize: 16, letterSpacing: 2.2, color: Colors.black)),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 40,
               ),
-            )
-          ],
-        ),
-      ),
+              Row(
+                children: [
+                  Icon(
+                      Icons.person,
+                      color: const Color(0xFF0D4065)
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    "Account",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              Divider(
+                height: 15,
+                thickness: 2,
+              ),
+              SizedBox(height:20.0),
+              ExpansionTile(
+                title: Text(
+                  "Change Password",
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                     color: Colors.grey,
+                  ),
+                ),
+                children: <Widget>[
+
+                  ListTile(
+                    title: Text('Current Password'), // Correct usage of title
+                    subtitle: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Enter your current password',
+                        border: OutlineInputBorder(),
+                      ),
+                      obscureText: true, // For password input
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _currentPassword = value!;
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ListTile(
+                    title: Text('New Password'),
+                    subtitle: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Enter your new password',
+                        border: OutlineInputBorder(),
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your new password';
+                        }
+                        if (value.length < 6) {
+                          return 'Password must be at least 6 characters long';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _newPassword = value!;
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ListTile(
+                    title: Text('Confirm New Password'),
+                    subtitle: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Confirm your new password',
+                        border: OutlineInputBorder(),
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please confirm your new password';
+                        }
+                        if (value != _newPassword) {
+                          return 'Passwords do not match';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _confirmPassword = value!;
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        // Process the password, e.g., send it to the server or authenticate locally
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Processing Password: $_currentPassword')),
+                        );
+                      }
+                    },
+
+                    child: Text('Confirm'),
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text(
+                  "Change Email Address",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey,
+                  ),
+                ),
+                children: <Widget>[
+
+                  ListTile(
+                    title: Text('Current Email'),
+                    subtitle: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Enter your current email',
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your current email';
+                        }
+                        if (!RegExp(_emailPattern).hasMatch(value)) {
+                          return 'Please enter a valid email address';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _currentEmail = value!;
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ListTile(
+                    title: Text('New Email'),
+                    subtitle: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Enter your new email',
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your new email';
+                        }
+                        if (!RegExp(_emailPattern).hasMatch(value)) {
+                          return 'Please enter a valid email address';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _newEmail = value!;
+                      },
+                    ),
+                  ),
+    SizedBox(height: 20),
+    ElevatedButton(
+    onPressed: () {
+    if (_formKey.currentState!.validate()) {
+    _formKey.currentState!.save();
+    // Process the emails, e.g., send to the server or authenticate locally
+    ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(content: Text('Email change successful')),
     );
+    }
+    },
+
+    child: Text('Confirm'),
+    ),
+
+
+            ],
+              ),
+              ExpansionTile(
+                title: Text(
+                  "App Settings",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey,
+                  ),
+                ),
+                children: <Widget>[
+
+                  ListTile(
+                    title: Text('Dark Mode'),
+                    trailing: Switch(
+                      value: _darkModeEnabled,
+                      onChanged: (bool value) {
+                        setState(() {
+                          _darkModeEnabled = value;
+                        });
+                      },
+                      activeColor: const Color(0xFF0D4065), // Correctly placed within the Switch widget
+                    ),
+                  ),
+
+                  ListTile(
+                    title: Text('Volume'),
+                    subtitle: Slider(
+                      value: _volumeLevel,
+                      min: 0.0,
+                      max: 1.0,
+                      divisions: 10,
+                      label: (_volumeLevel * 100).toStringAsFixed(0) + '%',
+                      onChanged: (double value) {
+                        setState(() {
+                          _volumeLevel = value;
+                        });
+                      },
+                      activeColor: const Color(0xFF0D4065), // Correctly placed within the Slider widget
+                    ),
+                  ),
+
+                  ListTile(
+                    title: Text('Language'),
+                    subtitle: DropdownButton<String>(
+                      value: _language,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _language = newValue!;
+                        });
+                      },
+                      items: <String>['English', 'Spanish', 'French', 'German']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+
+                    ],
+                  ),
+              SizedBox(
+                height: 40,
+              ),
+              Row(
+                children: [
+                  Icon(
+                      Icons.notifications,
+                      color: const Color(0xFF0D4065)
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    "Notifications",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              Divider(
+                height: 15,
+                thickness: 2,
+              ),
+
+              SizedBox(
+                height: 10,
+              ),
+              buildNotificationOptionRow("New for you", true),
+              buildNotificationOptionRow("Account activity", true),
+              buildNotificationOptionRow("Extend dates", false),
+              SizedBox(
+                height: 50,
+              ),
+              Center(
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text("SIGN OUT",
+                      style: TextStyle(
+                          fontSize: 16, letterSpacing: 2.2, color: Colors.black )),
+                ),
+              )
+                ],
+              ),
+
+
+
+        ),
+        ]
+          ),
+          ),
+
+        );
+
+
   }
 
   Row buildNotificationOptionRow(String title, bool isActive) {
@@ -163,40 +389,3 @@ class _SettingsPageState extends State<SettingsPage> {
 
 
 
-    class Item {
-    Item({
-    required this.expandedValue,
-    required this.headerValue,
-    this.isExpanded = false,
-    });
-
-    String expandedValue;
-    String headerValue;
-    bool isExpanded;
-    }
-
-    List<Item> generateItems() {
-    return [
-    Item(
-    headerValue: 'Change password',
-    expandedValue: 'change your password here',
-    ),
-    Item(
-    headerValue: 'Content settings',
-    expandedValue: 'Details about content settings',
-    ),
-    Item(
-    headerValue: 'Email settings',
-    expandedValue: 'Details about email settings',
-    ),
-    Item(
-    headerValue: 'Language',
-    expandedValue: 'Details about language settings',
-    ),
-    Item(
-    headerValue: 'Privacy and security',
-    expandedValue: 'Details about privacy and security settings',
-    ),
-    ];
-
-  }
