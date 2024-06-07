ca255e1bf2ae7d40359cb73166c8be281e2f5aab
Godagesasindu <godagesasindu@gmail.com>
2024-06-07 23:03:32 +0530

    Add Search Resources

--- lib/screens/search_resource.dart
+++ lib/screens/search_resource.dart
@@ -127,9 +127,11 @@ import 'package:easylibro_app/widgets/search__bar.dart';
 import 'package:flutter/material.dart';
 
 class SearchResource extends StatefulWidget {
+
   const SearchResource({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _SearchResourceState createState() => _SearchResourceState();
 }
 
@@ -141,59 +143,68 @@ class _SearchResourceState extends State<SearchResource> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: const Color(0xFFF7F8FD),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisSize: MainAxisSize.min, // Set mainAxisSize to min
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Search_Bar(hintText: "Search Resources"),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: Container(
-                        height: 40,
-                        width: 40,
-                        decoration: BoxDecoration(
-                          color: const Color(0xFF0D4065),
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        child: const Icon(
-                          Icons.tune_outlined,
-                          color: Colors.white,
-                          size: 25,
-                        ),
+        body: Column(
+            children: [
+            Padding(
+              padding: const EdgeInsets.only(left:20,right: 20,top: 10,bottom: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Search_Bar(hintText: "Search Resources"),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF0D4065),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: const Icon(
+                        Icons.tune_outlined,
+                        color: Colors.white,
+                        size: 25,
                       ),
                     ),
+                  ),
+                ],
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    _buildResourceCategory(index: 0, name: "All Resources"),
+                    _buildResourceCategory(index: 1, name: "Latest"),
+                    _buildResourceCategory(index: 2, name: "Popular"),
                   ],
                 ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      _buildResourceCategory(index: 0, name: "All Resources"),
-                      _buildResourceCategory(index: 1, name: "Latest"),
-                      _buildResourceCategory(index: 2, name: "Popular"),
-                      _buildResourceCategory(index: 3, name: "Recommended"),
-                      _buildResourceCategory(index: 4, name: "Popular"),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 20),
-                // Flexible(
-                //   fit: FlexFit.loose,
-                //   child: isSelected == 0
-                //       ? _buildAllResources()
-                //       : _buildLatestResources(),
-                // ),
-                Expanded(child: _buildAllResources())
-              ],
+              ),
             ),
-          ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left:20,right: 20),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height-300,
+                child: isSelected == 0
+                    ? _buildAllResources()
+                    : isSelected == 1
+                        ? _buildLatestResources()
+                        : _buildPopularResources(),
+              ),
+            )
+            
+            
+            // Expanded(child: isSelected == 0
+            //         ? _buildAllResources()
+            //         : isSelected == 1
+            //             ? _buildLatestResources()
+            //             : _buildPopularResources(),)
+          ],
         ),
       ),
     );
@@ -209,8 +220,9 @@ class _SearchResourceState extends State<SearchResource> {
         child: Container(
           width: 105,
           height: 40,
-          margin: const EdgeInsets.only(top: 20, right: 30),
+          margin: const EdgeInsets.only(top: 10, right: 20),
           padding: const EdgeInsets.all(8),
+          
           alignment: Alignment.center,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(15),
@@ -233,13 +245,15 @@ class _SearchResourceState extends State<SearchResource> {
 
   Widget _buildAllResources() => GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
+            crossAxisCount: 3,
             crossAxisSpacing: 12,
-            mainAxisSpacing: 10,
-            childAspectRatio: 4 / 5),
-        // shrinkWrap: true,
-        // physics: const NeverScrollableScrollPhysics(),
+            mainAxisSpacing: 12,
+             childAspectRatio: (1 / 2)
+           ),
+        shrinkWrap: true,
+        //physics: BouncingScrollPhysics(),
         scrollDirection: Axis.vertical,
+        physics: AlwaysScrollableScrollPhysics(),
         itemCount: MyResources.allResources.length,
         itemBuilder: (context, index) {
           final allResources = MyResources.allResources[index];
@@ -249,17 +263,32 @@ class _SearchResourceState extends State<SearchResource> {
 
   Widget _buildLatestResources() => GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: (100 / 140),
+          crossAxisCount: 3,
+          childAspectRatio: (1 / 2),
           crossAxisSpacing: 12,
           mainAxisSpacing: 100,
         ),
         shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
+        physics: AlwaysScrollableScrollPhysics(),
         itemCount: MyResources.latest.length,
         itemBuilder: (context, index) {
           final latest = MyResources.latest[index];
           return ResourceCard(resource: latest);
+        },
+      );
+  Widget _buildPopularResources() => GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3, 
+           childAspectRatio: (1 / 2),
+          crossAxisSpacing: 12,
+          mainAxisSpacing: 100,
+        ),
+        shrinkWrap: true,
+        physics: AlwaysScrollableScrollPhysics(),
+        itemCount: MyResources.popular.length,
+        itemBuilder: (context, index) {
+          final popular = MyResources.popular[index];
+          return ResourceCard(resource: popular);
         },
       );
 }
