a47705b4e6d77c2859b2fb105c76116bb386e2d0
Godagesasindu <godagesasindu@gmail.com>
2024-06-23 00:14:51 +0530

    comit

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'package:easylibro_app/widgets/theme_data_style.dart';
+//import 'package:easylibro_app/widgets/theme_data_style.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
