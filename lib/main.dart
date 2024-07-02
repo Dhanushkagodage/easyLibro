fd0b25b89b8478c9347bfe7df46123dcaeb131ab
ushaeni <139000191+ushaeni@users.noreply.github.com>
2024-07-02 11:45:17 +0530

    set_token

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,61 @@
 import 'package:easylibro_app/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:easylibro_app/firebase_options.dart';
+import 'package:easylibro_app/api/firebase_api.dart';
+// import 'package:firebase_analytics/firebase_analytics.dart';
+// import 'package:firebase_analytics/observer.dart';
+//
+//
+// void main() async{
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+//   await FirebaseApi().initNotifications();
+//
+//   FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+//
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: "EasyLibro APP",
+//       home: SplashScreen(),
+//     );
+//   }
+// }
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseApi().initNotifications();
 
-void main() {
-  runApp(const MyApp());
+  FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+
+  runApp(MyApp(analytics: analytics));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final FirebaseAnalytics analytics;
+
+  const MyApp({Key? key, required this.analytics}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "EasyLibro APP",
       home: SplashScreen(),
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: analytics),
+      ],
     );
   }
 }
